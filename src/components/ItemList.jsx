d47a3455ad07a271b6136e24f4f40431f3780ac1
espadv69 <aitorjob69@gmail.com>
2025-01-21 12:40:04 +0100

    Enhance no items found message with red color and center alignment

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,9 @@ import Item from './Item.jsx'
 import '../css/ItemList.css'
 
 const ItemList = ({ items, onDeleteItem }) => {
-  if (!items || items.length === 0) return <p>No items found!</p>
+  if (!items || items.length === 0) {
+    return <p style={{ color: 'red', textAlign: 'center' }}>No items found!</p>
+  }
 
   return (
     <ul className="ul-list">
