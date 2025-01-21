a595924f14b3a95de7a99a3c746c9ef205408c18
espadv69 <aitorjob69@gmail.com>
2025-01-21 12:34:51 +0100

    Add CSS import and class to ItemList component for styling

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,11 @@
 import Item from './Item.jsx'
+import '../css/ItemList.css'
 
 const ItemList = ({ items, onDeleteItem }) => {
   if (!items || items.length === 0) return <p>No items found!</p>
 
   return (
-    <ul>
+    <ul className="ul-list">
       {items.map((item) => (
         <Item key={item.id} item={item} onDelete={onDeleteItem} />
       ))}
